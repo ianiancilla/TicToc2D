30b2c9b1d4ead555c56dcd502fcadd9f688e5029
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2020-05-07 17:15:32 +0200

    camera can zoom out/in

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,50 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] bool zoomedOut = false;
+    [SerializeField] [Tooltip("Orthographic size of the camera when zoomed in")]
+                     float zoomedInSize = 4.06f;
+    [SerializeField] [Tooltip("Orthographic size of the camera when zoomed out")]
+                     float zoomedOutSize = 10f;
+    [SerializeField] float zoomSpeed = 1f;
+
+    // cache
+    float orthographicSize;
+    CinemachineVirtualCamera virtualCamera;
+
+    private void Start()
+    {
+        // cache
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+    private void Update()
+    {
+        var deltaZoom = zoomSpeed * Time.deltaTime;
+        if (zoomedOut)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Clamp(virtualCamera.m_Lens.OrthographicSize + deltaZoom,
+                                                                virtualCamera.m_Lens.OrthographicSize,
+                                                                zoomedOutSize);
+        }
+        else
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Clamp(virtualCamera.m_Lens.OrthographicSize - deltaZoom,
+                                                                zoomedInSize,
+                                                                virtualCamera.m_Lens.OrthographicSize);
+        }
+    }
+
+    public void ZoomIn()
+    {
+        zoomedOut = false;
+    }
+
+    public void ZoomOut()
+    {
+        zoomedOut = true;
+    }
+}
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -7,14 +7,17 @@ public class InputHandler : MonoBehaviour
     // variables
     float horInput;
     float verInput;
+    float spaceBarPressed;
 
     // cache
     CharaController charaController;
+    CameraZoom cameraZoom;
 
     private void Start()
     {
         // cache
         charaController = GetComponent<CharaController>();
+        cameraZoom = FindObjectOfType<CameraZoom>();
     }
 
     // Update is called once per frame
@@ -22,13 +25,22 @@ public class InputHandler : MonoBehaviour
     {
         horInput = Input.GetAxis("Horizontal");
         verInput = Input.GetAxis("Vertical");
+        spaceBarPressed = Input.GetAxis("Jump");
 
-
+        if (spaceBarPressed > 0)
+        {
+            cameraZoom.ZoomOut();
+        }
+        else
+        {
+            cameraZoom.ZoomIn();
+        }
     }
 
     private void FixedUpdate()
     {
         charaController.Move(horInput, 0);
         if (verInput != 0) { charaController.ClimbStairs(verInput); }
+
     }
 }
